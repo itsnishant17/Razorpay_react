f226deca727b45da472efb98e1c90c8b382dbcc1
Nishant Kumar <work.nishantkumar@gmail.com>
2023-03-27 11:53:15 +0530

    2nd commit

---
+++ frontend/src/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import{Box,Stack} from "@chakra-ui/react"
+const Home = () => {
+  return (
+    <Box>
+
+    <Stack>
+      
+    </Stack>
+
+    </Box>
+  )
+}
+
+export default Home
