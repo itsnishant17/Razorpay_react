45faaf635d41af85debee0ef17b33f274263b4ae
Nishant Kumar <work.nishantkumar@gmail.com>
2023-03-30 01:49:30 +0530

    6th commit

--- frontend/src/Card.jsx
+++ frontend/src/Card.jsx
@@ -4,9 +4,11 @@ import React from 'react'
 const Card = ({ amount, img, checkoutHandler }) => {
     return (
         <VStack>
+
             <Image src={img} boxSize={"64"} objectFit="cover" />
             <Text>₹{amount}</Text>
             <Button onClick={() => checkoutHandler(amount)}>Buy Now</Button>
+            
         </VStack>
     )
 }
